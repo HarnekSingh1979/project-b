d2092f2bc10abaa5951ae4a95a139f3c0a24da83
Harnek Singh Desktop <harnek1979@gmail.com>
2021-05-13 17:13:09 +0530

    Harnoor project file added

---
+++ Harnoor Singh 7th C Test part 2.py
@@ -0,0 +1,8 @@
+str =input("Enter the Alphabet :")
+if( str=="a" or "e" or "i" or "o" or "u" or "A" or "E" or "I" or "O" or "U"):
+  print("The Alphabet is a Vowel")
+else:
+    print("The Alphabet is a Consonant")
+input(" PRESS ANY KEY TO CLOSE")
+
+    
